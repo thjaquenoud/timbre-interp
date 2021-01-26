f11023fd08f858f37bcd06bcb5430baf5e9e7182
maltzs <51710165+maltzs@users.noreply.github.com>
2021-01-26 16:27:20 -0500

    Add files via upload
    training files

---
+++ training/effects/slurmEffectsVar.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=MUSIC_TEST
+#SBATCH --output=/zooper2/musicae/effects/outputVar.txt
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks=1
+#SBATCH --gres-flags=enforce-binding
+#SBATCH --nodes=1-1
+#SBATCH --mem=1gb
+
+#This is a sample SLURM job script
+#It has a job name called JOB_NAME
+#It will save it's stdout to /zooper2/USERNAME/output.txt
+#It will have access to 2 GPUs and 1GB of RAM for a maximum of 1 minute
+#The minimum allocatable time is 1 minute
+
+echo "CUDA DEVICES: $CUDA_VISIBLE_DEVICES"
+export CUDA_HOME="/usr/local/cuda-10.1"
+export PATH="$PATH:/usr/local/cuda-10.1/bin"
+export LD_LIBRARY_PATH="/usr/local/cuda-10.1/lib64:/usr/local/cuda/extras/CUPTI/lib64"
+source ../venv/bin/activate
+python train-network-eff-var.py --filename_in=long_frames --filename_out=eff_test --n_epochs=20
+
